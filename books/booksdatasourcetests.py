d53405b07506b1f9be55d8f3e168e3727ea73e7d
Kyle Machalec <machaleck@Kyle_XPS>
2022-09-22 12:09:49 -0500

    Added 3 tests

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -1,6 +1,8 @@
 '''
    booksdatasourcetest.py
    Jeff Ondich, 24 September 2021
+
+   Adapted by Kyle Machalec and Muno Siyakurima
 '''
 
 from booksdatasource import Author, Book, BooksDataSource
@@ -17,6 +19,27 @@ class BooksDataSourceTester(unittest.TestCase):
         authors = self.data_source.authors('Pratchett')
         self.assertTrue(len(authors) == 1)
         self.assertTrue(authors[0] == Author('Pratchett', 'Terry'))
+
+    def test_unique_book(self):
+        books = self.data_source.books('Blackout')
+        self.assertTrue(len(books) == 1)
+        self.assertTrue(books[0] == Book('Blackout'))
+
+    def test_unique_book_numbers(self):
+        books = self.data_source.books('1Q84')
+        self.assertTrue(len(books) == 1)
+        self.assertTrue(books[0] == Book('1Q84'))
+
+    def test_unique_book_repeat_title(self):
+        books = self.data_source.books('and')
+        self.assertTrue(len(books) == 7)
+        self.assertTrue(books[0] == Book('And Then There Were None'))
+        self.assertTrue(books[1] == Book('Boys and Sex'))
+        self.assertTrue(books[2] == Book('Girls and Sex'))
+        self.assertTrue(books[3] == Book('Hard-Boiled Wonderland and the End of the World'))
+        self.assertTrue(books[4] == Book('Pride and Prejudice'))
+        self.assertTrue(books[5] == Book('Sense and Sensibility'))
+        self.assertTrue(books[6] == Book('\"The Life and Opinions of Tristram Shandy, Gentleman\"'))
 
 if __name__ == '__main__':
     unittest.main()
