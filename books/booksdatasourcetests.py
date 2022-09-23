2415edcebfc9e797c54bf63c19b657b52b10b619
Muno Siyakurima <siyakurimam@carleton.edu>
2022-09-23 09:31:42 -0500

    Added tests to booksdatasourcetests.py

--- books/booksdatasourcetests.py
+++ books/booksdatasourcetests.py
@@ -18,6 +18,27 @@ class BooksDataSourceTester(unittest.TestCase):
         self.assertTrue(len(authors) == 1)
         self.assertTrue(authors[0] == Author('Pratchett', 'Terry'))
 
+    def test_unique_book(self):
+        books = self.data_source.books('Blackout')
+        self.assertTrue(len(books) == 1)
+        self.assertTrue(books[0] == Book('Blackout'))
+
+    def test_unique_book_numbers(self):
+        books = self.data_source.books('1Q84')
+        self.assertTrue(len(books) == 1)
+        self.assertTrue(books[0] == Book('1Q84'))
+
+    def test_unique_book_repeat_title(self):
+        books = self.data_source.books('and')
+        self.assertTrue(len(books) == 7)
+        self.assertTrue(books[0] == Book('And Then There Were None'))
+        self.assertTrue(books[1] == Book('Boys and Sex'))
+        self.assertTrue(books[2] == Book('Girls and Sex'))
+        self.assertTrue(books[3] == Book('Hard-Boiled Wonderland and the End of the World'))
+        self.assertTrue(books[4] == Book('Pride and Prejudice'))
+        self.assertTrue(books[5] == Book('Sense and Sensibility'))
+        self.assertTrue(books[6] == Book('\"The Life and Opinions of Tristram Shandy, Gentleman\"'))
+
     def test_all_books(self):
         tiny_data_source = BooksDataSource('tinybooks.csv')
         books = tiny_data_source.books()
@@ -26,6 +47,15 @@ class BooksDataSourceTester(unittest.TestCase):
         self.assertTrue(books[1].title == 'Neverwhere')
         self.assertTrue(books[2].title == 'Omoo')
 
+    def test_all_authors(self):
+        tiny_data_source = BooksDataSource('tinybooks.csv')
+        authors = tiny_data_source.authors()
+        self.assertTrue(len(authors) == 3)
+        self.assertTrue(authors[0] == Author('Austen', 'Jane'))
+        self.assertTrue(authors[1] == Author('Gaiman', 'Neil'))
+        self.assertTrue(authors[2] == Author('Melville', 'Herman'))
+
+        
 if __name__ == '__main__':
     unittest.main()
 
